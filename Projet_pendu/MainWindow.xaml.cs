a4fe66577ad9680bd606e894cbdf0a2d16958340
Axel HEGO <axel.hego.thb@gmail.com>
2021-12-13 14:57:37 +0100

    MAJ

--- Projet_pendu/MainWindow.xaml.cs
+++ Projet_pendu/MainWindow.xaml.cs
@@ -24,17 +24,43 @@ namespace Projet_pendu
     {
         List<string> listMot = new List<string>();
         List<char> faussesLettres = new List<char>();
-        int motATrouver = 2;
+        List<Image> pendu = new List<Image>();
+        int motATrouver = 0;
         string motCacher;
 
         public MainWindow()
         {
             InitializeComponent();
+            pendu.Add(erreur1);
+            pendu.Add(erreur2);
+            pendu.Add(erreur3);
+            pendu.Add(erreur4);
+            pendu.Add(erreur5);
+            pendu.Add(erreur6);
+            pendu.Add(erreur7);
+            pendu.Add(erreur8);
+            pendu.Add(erreur9);
             listMot.Add("U T I L I S A T E U R");
             listMot.Add("S Y S T E M E");
             listMot.Add("B I N A I R E");
             motCacher = CacherMot(listMot[motATrouver]);
-            labelMotATrouver.Content = motCacher;
+            labelMot.Content = motCacher;
+        }
+
+        private void setDefaultWindow()
+        {
+            for(int i = 0; i<pendu.Count(); i++)
+            {
+                pendu[i].SetValue(Panel.ZIndexProperty, 0);
+            }
+            for (int i = faussesLettres.Count() - 1; i >= 0; i--)
+            {
+                faussesLettres.RemoveAt(i);
+            }
+            motATrouver = 0;
+            motCacher = CacherMot(listMot[motATrouver]);
+            Trace.WriteLine(motCacher);
+            labelLettresFausses.Content = "";
         }
 
         public bool Comparer_lettre(string motATrouver, char lettre)
@@ -55,7 +81,6 @@ namespace Projet_pendu
             StringBuilder sb = new StringBuilder(motACacher);
             for (int i = 0; i < motACacher.Length; i++)
             {
-                Trace.WriteLine(sb[i]);
                 if (sb[i] != ' ')
                 {
                     sb[i] = '-';
@@ -79,50 +104,77 @@ namespace Projet_pendu
                         sb[i] = lettre;
                         res = 1;
                     }
-
+                    motCacher = sb.ToString();
                 }
             }
             if (res == 0)
             {
-                faussesLettres.Add(lettre);
-                afficher_LettreFausses();
+                bool dejaEssayer = false;
+                for(int i = 0; i < faussesLettres.Count(); i++)
+                {
+                    if(lettre == faussesLettres[i])
+                    {
+                        dejaEssayer = true;
+                    }
+                }
+                if(!dejaEssayer)
+                {
+                    faussesLettres.Add(lettre);
+                    afficher_LettreFausses();
+                    afficher_pendu(faussesLettres.Count());
+                    if(faussesLettres.Count() == 9)
+                    {                        
+                        MessageBox.Show("Vous avez perdu ! Fermez la fenêtre pour recommencer");
+                        setDefaultWindow();
+                    }
+                }
             }
-
-            motCacher = sb.ToString();
-
             if (motCacher == listMot[motATrouver])
             {
                 motATrouver++;
                 if (motATrouver <= 2)
                 {
                     motCacher = CacherMot(listMot[motATrouver]);
+                    for(int i = faussesLettres.Count()-1; i >= 0; i--)
+                    {
+                        faussesLettres.RemoveAt(i);
+                    }
+                    for (int i = 0; i < pendu.Count(); i++)
+                    {
+                        pendu[i].SetValue(Panel.ZIndexProperty, 0);
+                    }
+                    labelLettresFausses.Content = "";
                 }
                 else
                 {
-                    labelMotATrouver.Content = motCacher;
+                    labelMot.Content = motCacher;
                     Window1 newWin = new Window1();
                     newWin.ShowDialog();
                 }
             }
-            labelMotATrouver.Content = motCacher;
+            labelMot.Content = motCacher;
+        }
+
+        private void afficher_pendu(int nb_erreur)
+        {
+            pendu[nb_erreur-1].SetValue(Panel.ZIndexProperty, 3);
         }
 
         private void input_jeux_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key.ToString() == "Return")
             {
-                if (input_jeux.Text.Length <= 1)
+                if (input_jeux.Text.Length == 1)
                 {
                     Traitement(Convert.ToChar(input_jeux.Text.ToUpper()));
                     input_jeux.Text = "";
                 }
             }
-
         }
 
         private void TestBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (input_jeux.Text.Length <= 1)
+            if (input_jeux.Text.Length == 1)
             {
                 Traitement(Convert.ToChar(input_jeux.Text.ToUpper()));
                 input_jeux.Text = "";
@@ -141,13 +193,3 @@ namespace Projet_pendu
         }
     }
 }
-
-
-/*
- A FAIRE !!!
-    Changer les entrées sorties par les évenements
-    Changer le type "size_t"
-    Changer la méthode string.size() par une methode compatible en c#
-    Changer la méthode string.push_back() par une methode compatible en c
-    Ajouter methode gagner() 
-     */
